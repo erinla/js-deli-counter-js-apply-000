25f4bcbb0e1a900f5170628845328889ad6ad213
Erin Lawler <erin_lawler@yahoo.com>
2019-03-26 00:56:21 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,5 @@
+var katzDeliLine = ["Ada", "Grace", "Kent"];
+
+for (i = 0; i < katzDeliLine.length; i++){
+console.log("Welcome, " + katzDeliLine[i] + ". You are number " + [i+1] + " in line.");
+}
