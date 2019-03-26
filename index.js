f3ab6ce34bd5ca470907f502496931dc7d81281f
Erin Lawler <erin_lawler@yahoo.com>
2019-03-26 02:00:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(line) {
   else {
     var result = ("The line is currently: ")
     for (var i = 0; i < line.length; i++) {
-      result += (i+1) + "." + line[i]+", "
+      result += (i+1) + ". " + line[i]+", "
     }
     return result
   }
